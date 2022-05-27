e2b02e5722d367b1068d9e06a5dec440f67ff24c
budibaen <106385415+budibaen@users.noreply.github.com>
2022-05-27 21:56:54 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// A Pure CSS Mario 64
+
+// No images, No JavaScript, just CSS
+
+// iOS Safari doesn't like it
+
+// instagram.com/ivorjetski
+// twitter.com/ivorjetski
+// youtube.com/c/ivorjetski
